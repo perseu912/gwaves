37a8a1e2b69fc088454840354ce40f44c7509e81
perseu912 <perseu912@gmail.com>
2021-12-29 20:10:31 -0300

    making the rst for readthedocs

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from setuptools import find_packages, setup
+
+with open("README.rst", "r") as fh:
+    readme = fh.read()
+
+setup(name='gwaves',
+    version='0.0.1b',
+    url='https://github.com/perseu912/gwaves',
+    license='MIT License',
+    author='Reinan Br',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='slimchatuba@gmail.com',
+    keywords='kit tools dev works',
+    description=u'Library for getting music in high quality from YouTube',
+    packages=find_packages(),
+    install_requires=['scipy','requests','pandas','bs4','h5py','matplotlib','numpy','soundfile','astropy','openpyxl','lxml'],)
---
+++ test_data_gw.py
@@ -0,0 +1,5 @@
+from gwaves import Gwaves_Data
+
+gw = Gwaves_Data()
+
+print(gw.data_gw)
